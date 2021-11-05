d22b775bf4782948221168e7ba4f1a652f13d543
CesarRawr <rawrcesar13@gmail.com>
2021-11-05 09:49:40 -0600

    rwr

---
+++ src/portal1.gd
@@ -0,0 +1,7 @@
+extends Area2D
+
+export var scene = "default"
+
+func _on_Next_body_entered(body):
+	if body.name == "Player":
+		get_tree().change_scene("res://src/scenes/"+ scene +".tscn")
---
+++ src/scripts/Player.gd
@@ -0,0 +1,45 @@
+extends KinematicBody2D
+
+
+# Declare member variables here.
+export var speed = 250  # How fast the player will move (pixels/sec).
+var screen_size  # Size of the game window.
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	screen_size = get_viewport_rect().size
+
+func _physics_process(delta):
+	move_and_collide(Vector2(0, 0))
+	pass
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var velocity = Vector2()  # The player's movement vector.
+	if Input.is_action_pressed("ui_right"):
+		velocity.x += 1
+	if Input.is_action_pressed("ui_left"):
+		velocity.x -= 1
+	if Input.is_action_pressed("ui_down"):
+		velocity.y += 1
+	if Input.is_action_pressed("ui_up"):
+		velocity.y -= 1
+	if velocity.length() > 0:
+		velocity = velocity.normalized() * speed
+		$AnimatedSprite.play()
+	else:
+		$AnimatedSprite.stop()
+
+	position += velocity * delta
+	#position.x = clamp(position.x, 0, screen_size.x)
+	#position.y = clamp(position.y, 0, screen_size.y)
+	
+	if velocity.x > 0:
+		$AnimatedSprite.animation = "right"
+	elif velocity.x < 0:
+		$AnimatedSprite.animation = "left"
+	elif velocity.y > 0:
+		$AnimatedSprite.animation = "down"
+	elif velocity.y < 0:
+		$AnimatedSprite.animation = "up"
