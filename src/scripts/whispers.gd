3a3f947258d5012b1d90d0b5d640dfec77204561
CesarRawr <rawrcesar13@gmail.com>
2021-11-05 15:18:55 -0600

    Adjusting sound to scene 4

--- src/scripts/whispers.gd
+++ src/scripts/whispers.gd
@@ -13,7 +13,7 @@ func _ready():
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
 	x = get_parent().get_child(2).get_global_transform().get_origin().x
-	
+	# Aumentar o disminuir el sonido dependiendo de si el jugador se acerca o aleja de la entrada	
 	if x < actual:
 		actual = x
 		get_parent().get_child(11).volume_db = ((x/x)-0.90) + get_parent().get_child(11).volume_db
